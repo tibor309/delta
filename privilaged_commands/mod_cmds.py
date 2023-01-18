1a956e89d85c8876df21fa9560cf443e80413f51
Tibor Mercs <tibormercs8@gmail.com>
2023-01-18 18:18:28 +0000

    Added role commands

--- privilaged_commands/mod_cmds.py
+++ privilaged_commands/mod_cmds.py
@@ -87,6 +87,36 @@ class mod_cmds(commands.Cog):
         else:
             await user.move_to(channel)
             await ctx.respond(f"Moved {user.mention} to {channel.mention}", ephemeral=True)
+
+
+    # Give role to user
+    @discord.slash_command(name="usermod", description="Give a role to user", guild_only=True)
+    @discord.commands.default_permissions(manage_roles=True)
+    @discord.option("user", discord.Member, description="Select a user", required=True)
+    @discord.option("role", discord.Role, description="Select a role", required=True)
+    async def give_role(self, ctx, user: discord.Member, role: discord.Role):
+
+        try:
+            await user.add_roles(role, reason=f"Gave role by {ctx.author.name}#{ctx.author.discriminator}")
+        except:
+            await ctx.respond(f"Failed to give role\nprobably a higher role than mine, or {user.mention} already has that role", ephemeral=True)
+
+        await ctx.respond(f"Added the {role.mention} role to {user.mention}", ephemeral=True)
+
+
+    # Give role to user
+    @discord.slash_command(name="userdemod", description="Revoke a role from user", guild_only=True)
+    @discord.commands.default_permissions(manage_roles=True)
+    @discord.option("user", discord.Member, description="Select a user", required=True)
+    @discord.option("role", discord.Role, description="and select a role", required=True)
+    async def revoke_role(self, ctx, user: discord.Member, role: discord.Role):
+
+        try:
+            await user.remove_roles(role, reason=f"Removed role by {ctx.author.name}#{ctx.author.discriminator}")
+        except:
+            await ctx.respond(f"Failed to remove role\nprobably a higher role than mine, or {user.mention} doesn't have that role.", ephemeral=True)
+
+        await ctx.respond(f"Removed the {role.mention} role from {user.mention}", ephemeral=True)
  
         
 
