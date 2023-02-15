dc12fd2fb485fc7e82d25e64c1ba9055611e0f27
Tibor Mercs <tibormercs8@gmail.com>
2023-02-15 17:44:14 +0000

    Changed neofetch theme to smallfetch

--- commands/system.py
+++ commands/system.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from config import bot_id, invite_emoji
-import os, platform, psutil, cpuinfo
+import os, platform, psutil
 import datetime, time
 
 
@@ -15,50 +15,42 @@ class system_commands(commands.Cog):
     # Neofetch command
     @discord.slash_command(name="neofetch", description="Some info about the system")
     async def neofetch(self, ctx):
-        await ctx.defer(ephemeral=True)
+        await ctx.defer()
         view = discord.ui.View()
         invite = f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot%20applications.commands"
-        view.add_item(discord.ui.Button(label="invite me", style=discord.ButtonStyle.gray, emoji=invite_emoji, url=invite))
-    
+        view.add_item(discord.ui.Button(label="invite me", style=discord.ButtonStyle.gray, emoji=invite_emoji, url=invite)) 
+        
         current_time = time.time()
         difference = int(round(current_time - start_time))
-        uptime = str(datetime.timedelta(seconds=difference))
+        
+        mins, sec = divmod(difference, 60)
+        hour, mins = divmod(mins, 60)
+        days, hour = divmod(hour, 24)
+
+        uptime = f"{days} days, {hour} hours, {mins} minutes, {sec} seconds"
 
         host_name = platform.uname()[1]
-        cpu_name = cpuinfo.get_cpu_info()['brand_raw']
-        ram_usage = round(psutil.virtual_memory().used / (1024**3), 1)
-        total_ram = round(psutil.virtual_memory().total / (1024**3), 1)
+        ram_usage = int(psutil.virtual_memory().used / (1024**2))
+        total_ram = int(psutil.virtual_memory().total / (1024**2))
+        terminal = os.ctermid()
       
-        disk_usage = round(psutil.disk_usage("/").used / (1024**3), 1)
-        total_disk = round(psutil.disk_usage("/").total / (1024**3), 1)
         cogs = len(self.bot.cogs)
-        python_version = platform.python_version()
-        pycord_version = discord.__version__
         modules = len(dir())
         ping = round(self.bot.latency * 1000)
-        guilds = len(self.bot.guilds)
-        terminal = os.ctermid()
        
         name = ctx.author.name
         botname = self.bot.user.name
-        
+
         neofetch = (f"```ansi\n"
-        f"[2;34m[1;34m{name.lower()}[0m[2;34m[0m@[2;34m[1;34m{botname.lower()}[0m[2;34m[0m\n-----------------\n"
-        f"[2;34m[1;34mOS[0m[2;34m[0m: Fedora Linux 37 (Server Edition) x86_64\n"
-        f"[2;34m[1;34mHost[0m[2;34m[0m: {host_name}\n"
-        f"[2;34m[1;34mKernel[0m[2;34m[0m: 6.0.8-300.fc37.x86_64\n"
-        f"[2;34m[1;34mUptime[0m[2;34m[0m: {uptime} seconds\n"
-        f"[2;34m[1;34mPing[0m[2;34m[0m: {ping}ms\n"
-        f"[2;34m[1;34mPackages[0m[2;34m[0m: 798 (rpm), {modules} (py), {cogs} (cog)\n"
-        f"[2;34m[1;34mShell[0m[2;34m[0m: bash 5.2.9\n"
-        f"[2;34m[1;34mResolution[0m[2;34m[0m: 3840x2160\n"
-        f"[2;34m[1;34mPython version[0m[2;34m[0m: {python_version}\n"
-        f"[2;34m[1;34mPycord version[0m[2;34m[0m: {pycord_version}\n"
-        f"[2;34m[1;34mGuilds[0m[2;34m[0m: {guilds} guilds\n"
-        f"[2;34m[1;34mTerminal[0m[2;34m[0m: {terminal}\n"
-        f"[2;34m[1;34mCPU[0m[2;34m[0m: {cpu_name}\n"
-        f"[2;34m[1;34mMemory[0m[2;34m[0m: {ram_usage}GB / {total_ram}GB\n"
-        f"[2;34m[1;34mDisk[0m[2;34m[0m: {disk_usage}GB / {total_disk}GB\n\n\n```")
+        f"      [2;37m_____[0m      [2;34m[1;34m{name.lower()}[0m[2;34m[0m@[2;34m[2;34m[1;34m{botname.lower()}[0m[2;34m[0m[2;34m[0m\n"
+        f"     [2;37m/[0m  [2;37m __)[0m[2;34m\[0m    [2;34m[2;34m[1;34mOS[0m[2;34m[0m[2;34m[0m[2;37m:[0m Fedora Linux 37 (Server Edition)\n"
+        f"     [2;37m|  /[0m  [2;34m\ \[0m   [2;34m[1;34mHost[0m[2;34m[0m[2;37m:[0m {host_name}\n"
+        f"  [2;34m__[0m[2;37m_|  |[0m[2;37m_[0m[2;37m[2;34m_/ /[0m[2;37m[0m   [1;2m[1;34mUptime[0m[0m[2;37m:[0m {uptime}\n"
+        f" [2;34m/[0m [2;37m(_    _)[0m[2;34m_/[0m    [2;34m[1;34mPing[0;34m[0;37m:[0m {ping}ms\n"
+        f"[2;34m/ /[0m  [2;37m|  |[0m        [2;34m[2;34m[1;34mPackages[0m[2;34m[0m[2;34m[0m[2;37m:[0m 795 (rpm), {modules} (py), {cogs} (cog)\n"
+        f"[2;34m\ \[0m[2;37m__/  |[0m        [2;34m[2;34m[1;34mShell[0m[2;34m[0m[2;34m[0m[2;37m:[0m bash\n"
+        f" [2;34m\[0m[2;37m(_____/[0m        [2;34m[1;34mTerminal[0m[2;34m[0m[2;37m:[0m {terminal}\n"
+        f"                 [2;34m[1;34mMemory[0m[2;34m[0m[2;37m:[0m {ram_usage}MiB / {total_ram}MiB\n```")
         await ctx.followup.send(neofetch, view=view)
 
 
