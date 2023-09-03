952ddb4484aa497cad809b571a3fb272fed6f678
tibor309 <haxite@proton.me>
2023-09-03 12:26:38 +0000

    Reworked neofetch command again

--- commands/system.py
+++ commands/system.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from config import invite_emoji, code_emoji
 from config import code_link
-import psutil
 import time
 
 
@@ -14,7 +13,7 @@ class system_commands(commands.Cog):
 
 
     # Neofetch command
-    @discord.slash_command(name="neofetch", description="System info")
+    @discord.slash_command(name="neofetch", description="Show system info")
     async def neofetch(self, ctx: commands.Context) -> None:
         await ctx.defer()
         
@@ -26,9 +25,6 @@ class system_commands(commands.Cog):
         days, hour = divmod(hour, 24)
 
         uptime = f"{days} days, {hour} hours, {mins} minutes, {sec} seconds"
-
-        ram_usage = int(psutil.virtual_memory().used / (1024**2))
-        total_ram = int(psutil.virtual_memory().total / (1024**2))
       
         cogs = len(self.bot.cogs)
         modules = len(dir())
@@ -36,6 +32,8 @@ class system_commands(commands.Cog):
         
         name = ctx.author.name
         botname = self.bot.user.name
+        users = len(self.bot.users)
+        guilds = len(self.bot.guilds)
 
         view = discord.ui.View()
         invite = f"https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8&scope=bot%20applications.commands"
@@ -43,14 +41,15 @@ class system_commands(commands.Cog):
         view.add_item(discord.ui.Button(label="invite me", style=discord.ButtonStyle.link, emoji=invite_emoji, url=invite))
 
         neofetch = (f"```ansi\n"
-        f"      [2;37m_____[0m      \n"
-        f"     [2;37m/[0m  [2;37m __)[0m[2;34m\[0m    [2;34m[1;34m{name.lower()}[0m[2;34m[0m@[2;34m[2;34m[1;34m{botname.lower()}[0m[2;34m[0m[2;34m[0m\n"
-        f"     [2;37m|  /[0m  [2;34m\ \[0m   [2;34m[2;34m[1;34mOS[0m[2;34m[0m[2;34m[0m[2;37m:[0m Fedora Linux 38 (Server Edition)\n"
-        f"  [2;34m__[0m[2;37m_|  |[0m[2;37m_[0m[2;37m[2;34m_/ /[0m[2;37m[0m   [1;34mUptime[0m[0m[2;37m:[0m {uptime}\n"
-        f" [2;34m/[0m [2;37m(_    _)[1;2m[0m[2;34m_/[0m    [2;34m[1;34mPing[0;34m[0;37m:[0m {ping}ms\n"
-        f"[2;34m/ /[0m  [2;37m|  |[0m        [2;34m[2;34m[1;34mPackages[0m[2;34m[0m[2;34m[0m[2;37m:[0m {modules} (python), {cogs} (cog)\n"
-        f"[2;34m\ \[0m[2;37m__/  |[0m        [2;34m[1;34mMemory[0m[2;34m[0m[2;37m:[0m {ram_usage}MiB / {total_ram}MiB\n"
-        f" [2;34m\[0m[2;37m(_____/[0m        \n```")
+        f"      [2;37m_____[0m      [2;34m[1;34m{name.lower()}[0m[2;34m[0m[2;37m@[0m[2;34m[1;34m[0m[2;34m[0m[1;2m[1;34m{botname.lower()}[0m[0m\n"
+        f"     [2;37m/   __)[0m[2;34m\[0m    [1;2m[1;34mOS[0m[0m[1;2m[1;37m:[0m[0m [2;37mFedora Linux 38 (Server Edition)[0m\n"
+        f"     [2;37m|  /[0m  [2;34m\ \[0m   [1;2m[1;34m[0m[0m[1;2m[1;34m[1;34mUptime[0m[1;34m[0m[0m[1;2m[1;37m:[0m[0m [2;37m{uptime}[0m\n"
+        f"  [2;34m__[0m[2;37m_|  |_[0m[2;34m_/ /[0m   [1;2m[1;34mPackages[0m[0m[1;2m[1;37m:[0m[0m [2;37m{modules} (python), {cogs} (cog)[0m\n"
+        f" [2;34m/[0m [2;37m(_    _)[0m[2;34m_/[0m    [1;2m[1;34mUsers[0m[0m[1;2m[1;37m:[0m[0m [2;37m{users} ({guilds} servers)[0m\n"
+        f"[2;34m/ /[0m  [2;37m|  |[0m        [1;2m[1;34mPing[0m[0m[1;2m[1;37m:[0m[0m [2;37m{ping} ms[0m\n"
+        f"[2;34m\ \[0m[2;37m__/  |[0m\n"
+        f" [2;34m\[0m[2;37m(_____/[0m\n```")
+        
         await ctx.followup.send(neofetch, view=view, ephemeral=True)
 
 
