f3a4890da4aa3c607501dcae6cce46247b67c731
tibor309 <haxite@proton.me>
2023-10-04 08:55:49 +0000

    Changed icons again

--- config.py
+++ config.py
@@ -17,8 +17,12 @@ err_channel = os.getenv("ERR_CHANNEL") # Channel id for error logging (for bot a
 
 
 # Icons for embeds
-user_icon = "https://images.tibor309.repl.co/icons/delta/user.png" # for member specific commnds
-guild_icon = "https://images.tibor309.repl.co/icons/delta/guild.png" # icon for guild specific commands
+user_icon = "https://i.imgur.com/U2GPhRR.png" # for member specific commnds
+guild_icon = "https://i.imgur.com/dxQXe7H.png" # icon for guild specific commands
+
+# Reaction images
+no_perms? = "https://i.imgur.com/cnwdsLo.png"
+hi_mods = "https://tenor.com/view/hi-gif-25925938"
 
 # Emojis for commands
 # You can use custom emojis, but the bot needs access to the server to use them!
@@ -31,10 +35,10 @@ code_emoji = "🧪" # emoji for source code link
 
 # Messages
 # One message is randomly selected to be sent
-no_perm = ["nope", "you don't have perms for this!", "access denied", "You don't have enough perms for this", "skill issue", "https://image.tibor309.repl.co/memes/no_perms.png", "you don't have perms", "https://tenor.com/view/nuh-uh-beocord-no-lol-gif-24435520", "nuh uh!"]
+no_perm = ["nope", "you don't have perms for this!", "access denied", "You don't have enough perms for this", "skill issue", no_perms?, "you don't have perms", "https://tenor.com/view/nuh-uh-beocord-no-lol-gif-24435520", "nuh uh!"]
 bot_no_perm = ["I don't have perms!", "i don't have enough perms for this", "i can't help you with that. i don't have perms"]
 cmd_dms = ["You can't use this command here.", "No", "You can only use these commands in servers.", "Nope", "you can't use this command in dms!"]
-bot_join_msg = ["Hi everyone!", "Hi", "hi everyone", "hi", "Hello everyone!", "https://tenor.com/view/hi-gif-25925938"]
+bot_join_msg = ["Hi everyone!", "Hi", "hi everyone", "hi", "Hello everyone!", hi_mods]
 err_msg = ["something went wrong", "well that didn't work", "uh.. i got an error", "got an error", "something didn't work"]
 img_fail = ["I couldn't get the image", "Failed to get the image :(", "i couldn't download the image"]
 on_cooldown = ["You're on cooldown!", "hey slow down", "not yet!"]
