3bfd05a1d2504953a492802f7d2c52196e622b73
Tibor Mercs <tibormercs8@gmail.com>
2023-10-31 18:07:01 +0100

    Removed restarter script

--- utils/restarter.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-import os
-from os import system
-
-print("Restarting repl")
-os.system('kill 1') # get a new ip
-sleep(7) # wait a few seconds
-system("python main.py") # then start the bot
